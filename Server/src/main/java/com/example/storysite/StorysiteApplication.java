5a994f190357ee1c50585f3a09251e48b3bc82c3
anlttce182383 <anlttce182383@fpt.edu.vn>
2025-12-12 02:23:11 +0700

    Initializing server project

---
+++ Server/src/main/java/com/example/storysite/StorysiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.storysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorysiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorysiteApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/example/storysite/StorysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorysiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
